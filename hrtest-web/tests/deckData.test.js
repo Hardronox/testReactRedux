57b4cdfe0675e548e00544551b3bf7ed579fddd0
wizz92 <wizz92@gmail.com>
2018-01-26 02:09:07 +0300

    init

---
+++ hrtest-web/tests/deckData.test.js
@@ -0,0 +1,12 @@
+function sum(a, b) {
+  return a + b;
+}
+
+describe("pow", function() {
+
+
+  it("возводит в n-ю степень", function() {
+    expect(sum(1, 2)).toBe(3);
+  });
+
+});
---
+++ server-side/README.md
@@ -0,0 +1,1 @@
+Optional srever-side task code is supposed to go there. Please keep it as it is if you decide to skip server-side assignment.
